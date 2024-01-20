839d204a20926529b304fcdd3acb73c6eae048bd
thalessz <thalessouza1904@gmail.com>
2024-01-20 02:59:06 -0300

    modified:   server.js 	modified:   views/index.hbs 	new file:   views/noticia.hbs

--- server.js
+++ server.js
@@ -2,18 +2,46 @@ var mysql = require('mysql2/promise');
 const express = require('express');
 const app = express();
 
+
 app.set('view engine', 'hbs')
 app.set('views', './views')
 app.set('assets', './assets')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    async function get_data(){
+        try{
+            const connection = await mysql.createConnection({
+                user: 'root', database: 'jornal'
+            });
+            console.log('Conexão bem sucedida com o banco de dados')
+            let [results, fields] = await connection.query('select * from noticia where 1');
+            res.render('index', {results});
+            connection.end();
+            }catch(err)
+            {console.log(err)}
+    }
+    get_data();
+    
 });
 app.get('/noticia/:id', (req,res) => {
     let id= req.params.id;
+    res.send('noticia selecionada:' + id);
+    async function get_data(){
+        try{
+            const connection = await mysql.createConnection({
+                user: 'root', database: 'jornal'
+            });
+            console.log('Conexão bem sucedida com o banco de dados')
+            let [results, fields] = await connection.query('select * from noticia where id = '+id);
+            res.render('index', {results});
+            connection.end();
+            }catch(err)
+            {console.log(err)}
+    }
+    get_data();
 })
 app.get('*', (req,res) =>{
-    res.send("Destino não encontrado :(", 404)
+    res.status(404).send("Destino não encontrado :(")
 })
 
 const port=3000;
@@ -21,16 +49,3 @@ app.listen(port, ()=>{
     console.log("servidor rodando em http://localhost:"+ port);
 });
 
-async function main(){
-    try{
-    let connection = await mysql.createConnection({
-        user: 'root', database: 'jornal'
-    });
-    console.log('Conexão bem sucedida com o banco de dados')
-    let [results, fields] = await connection.query('select * from noticia where 1');
-    connection.end();
-    console.log(results);
-
-    }catch(err){console.err}
-}
-main();
